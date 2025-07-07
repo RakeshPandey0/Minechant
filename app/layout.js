76da3a6d89d182a2c2612c4523572bc6b9776639
SamratAdhikari <samratmetaladhikari@gmail.com>
2025-07-07 21:45:03 +0545

    initial commit

---
+++ app/layout.js
@@ -0,0 +1,30 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
+export const metadata = {
+    title: "Minechant",
+    description: "Minecraft Enchantment Guide",
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} 
+                antialiased flex justify-center items-center`}
+            >
+                {children}
+            </body>
+        </html>
+    );
+}
