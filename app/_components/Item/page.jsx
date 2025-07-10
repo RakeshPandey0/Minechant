0e5234650519b8131de20742d0213b7ab31b6134
RakeshPandey0 <rp901522@gmail.com>
2025-07-10 15:00:50 +0545

    Item component made dynamic with name, level and image as props

--- app/_components/Item/page.jsx
+++ app/_components/Item/page.jsx
@@ -1,32 +1,31 @@
 import React from "react";
 import { Box } from "@mui/material";
 import Paper from "@mui/material/Paper";
-const page = () => {
+const page = ({itemName, level, image}) => {
   return (
-    <>
+    
       <div>
         <div className=" group/Item-box flex flex-wrap justify-center items-center w-35 h-35 m-1 bg-[#1C4464] border-4 rounded-xl">
           <div className="relative inline-block">
             <img
-              src="/assets/book.png"
-              alt="some alt"
+              src={image}
+              alt="image here"
               className="w-20 h-20 rounded-md object-cover "
             />
             <span className="absolute -top-2 -right-2 bg-red-600 text-white text-md font-bold px-2 py-0.5 rounded-full">
-              II
+              {level}
             </span>
           </div>
         </div>
         <div className="flex justify-center items-center flex-col text-2xl">
             <div>
-                Sharpness
+                {itemName}
             </div>
             <div>
-                II
+                {level}
             </div>
         </div>
       </div>
-    </>
   );
 };
 
