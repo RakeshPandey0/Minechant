690c0842bba758566915f66e0362bc727915a926
SamratAdhikari <samratmetaladhikari@gmail.com>
2025-07-10 12:37:25 +0545

    feat: add minecraft fonts

--- app/_components/Title/page.jsx
+++ app/_components/Title/page.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-const page = () => {
+const Page = () => {
     return (
-        <div className="fixed top-0 left-0 flex flex-col items-center justify-center h-20 w-full text-center m-4">
-            <h1 className="text-4xl font-bold mb-4">Minechant</h1>
-            <p className="text-lg text-gray-700">
+        <div className="fixed top-0 left-0 flex flex-col items-center justify-center h-20 w-full text-center m-4 p-4">
+            <h1 className="text-6xl font-bold mt-4 font-title">MINECHANT</h1>
+            <p className="text-lg text-gray-500">
                 Enchantment Guide for Minecraft!
             </p>
         </div>
     );
 };
 
-export default page;
+export default Page;
